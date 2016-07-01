cca45fd7d23298b19c1b4699f411efcc2a66b363
Coen Zimmerman <coen@pixelindustries.com>
2016-07-01 22:59:48 +0200

    small tweak for modelupdaterinterface

--- src/Contracts/ModelUpdaterInterface.php
+++ src/Contracts/ModelUpdaterInterface.php
@@ -2,6 +2,7 @@
 namespace Czim\NestedModelUpdater\Contracts;
 
 use Czim\NestedModelUpdater\Data\UpdateResult;
+use Czim\NestedModelUpdater\Exceptions\ModelSaveFailureException;
 use Illuminate\Database\Eloquent\Model;
 
 interface ModelUpdaterInterface
@@ -26,6 +27,8 @@ interface ModelUpdaterInterface
      * Creates a new model with (potential) nested data
      *
      * @param array $data
+     * @return UpdateResult
+     * @throws ModelSaveFailureException
      */
     public function create(array $data);
 
@@ -36,6 +39,7 @@ interface ModelUpdaterInterface
      * @param int|Model $model      either an existing model or its ID
      * @param string    $attribute  lookup column, if not primary key, only if $model is int
      * @return UpdateResult
+     * @throws ModelSaveFailureException
      */
     public function update(array $data, $model, $attribute = null);
 
