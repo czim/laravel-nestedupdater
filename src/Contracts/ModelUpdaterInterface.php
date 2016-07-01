564a4eeae41c824e43cfd7b5b446f4b77b525423
Coen Zimmerman <coen@pixelindustries.com>
2016-07-01 13:19:29 +0200

    added constructor to model updater interface

--- src/Contracts/ModelUpdaterInterface.php
+++ src/Contracts/ModelUpdaterInterface.php
@@ -8,6 +8,21 @@ interface ModelUpdaterInterface
 {
 
     /**
+     * @param string                      $modelClass      FQN for model
+     * @param null|string                 $parentAttribute the name of the attribute on the parent's data array
+     * @param null|string                 $nestedKey       dot-notation key for tree data (ex.: 'blog.comments.2.author')
+     * @param null|Model                  $parentModel     the parent model, if this is a recursive/nested call
+     * @param null|NestingConfigInterface $config
+     */
+    public function __construct(
+        $modelClass,
+        $parentAttribute = null,
+        $nestedKey = null,
+        Model $parentModel = null,
+        NestingConfigInterface $config = null
+    );
+
+    /**
      * Creates a new model with (potential) nested data
      *
      * @param array $data
